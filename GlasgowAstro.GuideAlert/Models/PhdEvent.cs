102ac3e698c3fd05c139f2a2b8705f2519f49f14
AF <andrew.feeney@screenmedia.co.uk>
2019-08-10 00:09:55 +0100

    initial commit

---
+++ GlasgowAstro.GuideAlert/Models/PhdEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlasgowAstro.GuideAlert.Models
+{
+    public class PhdEvent
+    {
+        public string Event { get; set; }
+
+        public double Timestamp { get; set; }
+
+        public string Host { get; set; }
+    }
+}
---
+++ GlasgowAstro.GuideAlert/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GlasgowAstro.GuideAlert
+{
+    class Program
+    {
+        const string Host = "localhost";
+        const int Port = 4400;
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*** Guide Alert by GlasgowAstro *** \n");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+
+            try
+            {
+                TcpClient client = new TcpClient(Host, Port);
+                NetworkStream stream = client.GetStream();
+                byte[] readBuffer = new byte[256];
+
+                while (true)
+                {
+                    var data = stream.Read(readBuffer, 0, readBuffer.Length);
+                    string stringData = Encoding.ASCII.GetString(readBuffer, 0, data);
+                    Console.WriteLine(stringData);
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO: Logging
+            }
+        }
+    }
+}
