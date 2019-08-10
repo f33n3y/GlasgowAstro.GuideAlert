06e3cf892fae3c86a3985ff95c631ad5f9827435
AF <andrew.feeney@screenmedia.co.uk>
2019-08-10 19:03:26 +0100

    comment phd event class

--- GlasgowAstro.GuideAlert/Models/PhdEvent.cs
+++ GlasgowAstro.GuideAlert/Models/PhdEvent.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GlasgowAstro.GuideAlert.Models
+﻿namespace GlasgowAstro.GuideAlert.Models
 {
+    /// <summary>
+    /// Represents an event notification message sent
+    /// by PHD server.
+    /// </summary>
     public class PhdEvent
     {
+        /// <summary>
+        /// The name of the event
+        /// </summary>
         public string Event { get; set; }
 
+        /// <summary>
+        /// Timestamp of event in seconds from
+        /// epoch, including fractional seconds
+        /// </summary>
         public double Timestamp { get; set; }
 
+        /// <summary>
+        /// Hostname of the machine running PHD
+        /// </summary>
         public string Host { get; set; }        
 
+        /// <summary>
+        /// Error message returned when star is lost
+        /// </summary>
         public string Status { get; set; }
     }
 }
